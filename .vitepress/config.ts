939d624bcc50d5c19ccef351a1369f866c2306c4
artdev-hash <kim.dev.pr@gmail.com>
2023-09-10 18:33:37 +0800

    dev.kimlor

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vitepress'
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
+  base: '/dev.kimlor',
   title: "My Awesome Project",
   description: "A VitePress Site",
   themeConfig: {
