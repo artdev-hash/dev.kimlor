8dc6c4e14b08a413c1fb4e462ef91eccbea45d6e
artdev-hash <kim.dev.pr@gmail.com>
2024-01-24 05:55:21 +0800

    v.1

--- .vitepress/theme/components/MyLayout.vue
+++ .vitepress/theme/components/MyLayout.vue
@@ -5,6 +5,9 @@
       <Category />
     </template>
     <template #doc-after>
+      <div>
+        <button @click="back">[cd 👓,] ....</button>
+      </div>
       <Comments />
     </template>
     <!-- Home slot-->
@@ -16,11 +19,40 @@
 </template>
 <script lang="ts" setup>
 import DefaultTheme from "vitepress/theme";
-const { Layout } = DefaultTheme;
 import HomeHero from "./HomeHero.vue";
 import CopyWright from "./CopyWright.vue";
 import Page from "./Page.vue";
 import Category from "./Category.vue";
 import Title from "./Title.vue";
+const { Layout } = DefaultTheme;
+const back = () => {
+  history.back();
+};
 </script>
-<style scoped></style>
+<style scoped>
+button {
+  display: inline-block;
+  position: relative;
+  color: var(--vp-c-color-d);
+  cursor: pointer;
+  font-size: 1.2em;
+  font-weight: bold;
+}
+
+button::after {
+  content: "";
+  position: absolute;
+  width: 100%;
+  transform: scaleX(0);
+  height: 2px;
+  bottom: 0;
+  left: 0;
+  background-color: var(--vp-c-color-d);
+  transform-origin: bottom right;
+  transition: transform 0.25s ease-out;
+}
+button:hover::after {
+  transform: scaleX(1);
+  transform-origin: bottom left;
+}
+</style>
